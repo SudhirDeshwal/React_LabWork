155d6cf171f9d0328b29dfbf1809c828c17f5e30
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-06-07 23:07:19 -0400

    files managed

--- src/App.js
+++ src/App.js
@@ -5,43 +5,26 @@ import Tasklist from './Tasklist';
 class App extends Component {
   state = {
 
-    tasklist: [
-      {
-        title: 'Humber',
-        tasks: ['Task #1' ,'Task #2', 'Task #3'],
-        id: 1,
-      },
-      {
-        title: 'MERN',
-        tasks: ['Lab' ,'Project', 'Quiz'],
-        id: 1,
-      },
-      {
-        title: 'Java',
-        tasks: ['GD' ,'Exam', 'Assignment'],
-        id: 1,
-      },
-
-    ],
+    tasklist: [ { title: 'Humber',tasks: ['Task #1' ,'Task #2', 'Task #3'],id: 1,},
+      {title: 'MERN',tasks: ['Lab' ,'Project', 'Quiz'],id: 1, },
+      {title: 'Java',tasks: ['GD' ,'Exam', 'Assignment'],id: 1,}, ],
   };
 
-   render(){
+render(){
 
     return(
       <div className='App'>
       <Header title="Task List" items="9"/>
 
-      { this.state.tasklist.map((t1) => (
+      {this.state.tasklist.map((t1) => (
 
-          <Tasklist title={t1.title} tasks={t1.tasks} />
+      <Tasklist title={t1.title} tasks={t1.tasks} />
       )
-      ) }
+      )}
       </div>
     );
   }
 }
 
-
-  
 
 export default App;
